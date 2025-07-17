0557ce16664d3231226618126aa17b07ad48b073
bdpoe <toyotdp@hotmail.com>
2025-07-16 23:24:39 -0500

    Agregando

--- src/db.js
+++ src/db.js
@@ -11,11 +11,10 @@ from './config.js'
 
 
 
-export const pool = createPool({
+const pool = createPool({
   host: process.env.DB_HOST,
-  port: process.env.DB_PORT,
   user: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
   database: process.env.DB_NAME,
-  ssl: true, // Esto es necesario en Railway PostgreSQL
-})
+  
+});
